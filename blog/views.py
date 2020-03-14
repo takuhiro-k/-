c8bf089009ebc17b48e1e86bab0dd03b2528fc84
taku <you@example.comlutier-of-japan@nike.eonet.ne.jp>
2020-03-15 04:56:07 +0900

    staticsから

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.db.models import Count, Q
 from django.http import Http404
 from django.views.generic.detail import DetailView
@@ -30,3 +30,34 @@ class CategoryListView(ListView):
 class TagListView(ListView):
     queryset = Tag.objects.annotate(num_posts=Count(
         'post', filter=Q(post__is_public=True)))
+
+class CategoryPostView(ListView):
+    model = Post
+    template_name = 'blog/category_post.html'
+
+    def get_queryset(self):
+        category_slug = self.kwargs['category_slug']
+        self.category = get_object_or_404(Category, slug=category_slug)
+        qs = super().get_queryset().filter(category=self.category)
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = self.category
+        return context
+
+
+class TagPostView(ListView):
+    model = Post
+    template_name = 'blog/tag_post.html'
+
+    def get_queryset(self):
+        tag_slug = self.kwargs['tag_slug']
+        self.tag = get_object_or_404(Tag, slug=tag_slug)
+        qs = super().get_queryset().filter(tags=self.tag)
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tag'] = self.tag
+        return context
