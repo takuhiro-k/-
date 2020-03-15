22bc1153dcd55191444bc0c9c61a0f2a1749dbc6
taku <you@example.comlutier-of-japan@nike.eonet.ne.jp>
2020-03-15 22:09:05 +0900

    フォーム追加から

--- mysite/settings.py
+++ mysite/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'mysite.context_processors.common',
             ],
         },
     },
@@ -122,7 +123,9 @@ USE_TZ = True
 MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'media_root')
 MEDIA_URL = '/media/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
